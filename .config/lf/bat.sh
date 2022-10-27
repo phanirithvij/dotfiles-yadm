96392f1dc14798c28f3a3254382af123fa7cead4
phanirithvij <phanirithvij2000@gmail.com>
2022-10-27 17:04:57 +0530

    .

--- .config/lf/bat.sh
+++ .config/lf/bat.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-bat $1 -P --color always --theme gruvbox-dark -p
+
+case "$1" in
+  *.tar*) tar tf "$1";;
+  *.zip) unzip -l "$1";;
+  *.jar) unzip -l "$1";;
+  *.apk) unzip -l "$1";;
+  *.rar) unrar l "$1";;
+  *.7z) 7z l "$1";;
+  *.pdf) pdftotext "$1" -;;
+  *.json) cat "$1" | jq -C;;
+  *) bat "$1" -P --color always --theme gruvbox-dark -p;;
+esac
+
