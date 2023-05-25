cea309ee40e032c578a2047bce31fe2832082253
phanirithvij <phanirithvij2000@gmail.com>
2023-05-25 15:21:52 +0530

    redis example lua, it is blocking so not useful

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,4 +1,42 @@
 pcall(require, "luarocks.loader")
+local debugp = require("gears.debug")
+--#region
+local lrc = require "lredis.cqueues"
+local cqueues = require "cqueues"
+
+-- Make a new cqueues scheduler
+local cq = cqueues.new()
+-- Make a thread that prints published messages to stdout
+cq:wrap(function()
+    local r = lrc.connect_tcp()
+    r:subscribe("quit")
+    r:psubscribe("b*")
+    while true do
+        local item = r:get_next()
+        if item == nil then break end
+        -- Can write `for item in r.get_next, r do` instead
+        -- but that doesn't work in lua5.1/luajit
+        local message_type = item[1]
+        if message_type == "message" then
+            debugp.print_error("Channel: " .. item[2] .. " Message: " .. item[3])
+            if item[2] == "quit" then break end
+        elseif message_type == "pmessage" then
+            debugp.print_error("Channel: " .. item[3] .. " Message: " .. item[4])
+        end
+    end
+end)
+-- Make a second thread that publishes events on an interval
+cq:wrap(function()
+    local r = lrc.connect_tcp()
+    for i=1, 10 do
+        cqueues.sleep(0.2)
+        r:call("publish", "bar", tostring(i))
+    end
+    r:call("publish", "quit", "")
+end)
+-- Start 'main' loop
+-- assert(cq:loop())
+--#endregion
 
 local awesome, client, screen = awesome, client, screen
 local ipairs, string, os, tostring, tonumber, type = ipairs, string, os, tostring, tonumber, type
